da3435baa5889fa2740d162a2a9f025ea88a7e71
Laura Gonçalves <143735022+lauragoncalvesf@users.noreply.github.com>
2024-06-05 15:28:29 -0300

    Update aula0.c
    mudando tamanho de vetor

--- aula0.c
+++ aula0.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 
 int main(void){
-int var = 2;
-int * point = &var;
-printf("Ponteiro %p e valor %d", p, *p);
-return 0;
+    int tamanho, novo_tamanho;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+    int * vetor = (int * ) calloc (tamanho, sizeof (int));
+    if(vetor == NULL)
+    {
+        printf("Nao ha espaço na memoria");
+        exit(1);
+    } else{
+        printf("vetor alocado!");
+    }       
+
+    printf("\nDigite um novo tamanho: ");
+    scanf("%d", &novo_tamanho);
+    vetor = (int* ) realloc (vetor, novo_tamanho*sizeof(int));
+    free(vetor);
+    return 0;
 }
