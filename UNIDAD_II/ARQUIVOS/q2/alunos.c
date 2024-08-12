869f500dc470521b1e63dfd1d7a492ad02187b25
lauragoncalvesf <lauragfernandespf@gmail.com>
2024-08-12 11:32:54 -0300

    att

--- UNIDAD_II/ARQUIVOS/q2/alunos.c
+++ UNIDAD_II/ARQUIVOS/q2/alunos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /*Função para abrir- criar arquivo e retornar este arquivo*/
 FILE * open_file(char * nome, char * modo){
     FILE * arquivo = fopen(nome, modo);
@@ -14,22 +15,22 @@ FILE * open_file(char * nome, char * modo){
 
 int main(void){
     FILE * arquivo_de_entrada, * arquivo_de_saida;
-    char * nome_aluno[20];
+    char nome_aluno[20];
     float nota1, nota2, nota3, media;
     arquivo_de_entrada = fopen("lista_alunos.txt", "r"); //Ler o arquivo de entrada
     arquivo_de_saida = open_file("saida_lista.txt", "w"); // Cria o arquivo de saída
 
     //Ler os dados dentro do arquivo fgets, fgetc, fscanf, sscanf
     while(!feof(arquivo_de_entrada)){ //enquanto não chegar ao final do arquivo
-    fscanf(arquivo_de_entrada, "%s\t%f\t%f\t%f\n", nome_aluno, &nota1, &nota2, &nota3); //ler os dados do arquivo
+        fscanf(arquivo_de_entrada, "%s\t%f\t%f\t%f\n", nome_aluno, &nota1, &nota2, &nota3); //ler os dados do arquivo
     // Calcular media
-    media = (nota1 + nota2 + nota3)/3.0;
+        media = (nota1 + nota2 + nota3)/3.0;
     // Escreve no arquivo de saída
-    if (media >= 7){
-        fprintf(arquivo_de_saida, "%s\t%f\tAPROVADO\n");
-    } else{
-        fprintf(arquivo_de_saida, "%s\t%f\tREPROVADO\n");
-    }    
+        if (media >= 7){
+        fprintf(arquivo_de_saida, "%s\t%f\tAPROVADO\n", nome_aluno, media);
+        } else{
+        fprintf(arquivo_de_saida, "%s\t%f\tREPROVADO\n",nome_aluno, media);
+        }    
     }
     fclose(arquivo_de_entrada);
     fclose(arquivo_de_saida);
